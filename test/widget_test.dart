5670d40706f46ed8e9847aff27bec377a6edb2a6
vibeeshan <vibeeshan@dh3s.com>
2024-08-23 17:59:01 +0530

    unit test issues

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,32 +1,120 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
+import 'package:eurovista/features/countryList/view/home.dart';
+import 'package:eurovista/features/countryList/viewmodel/ApiViewModel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:irusri_app/main.dart';
+import 'package:provider/provider.dart';
+import 'package:mockito/mockito.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 
 
 
+
+class MockApiViewModel extends Mock implements ApiViewModel {}
+
+class MockConnectivity extends Mock implements Connectivity {}
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  late MockApiViewModel mockApiViewModel;
+  late MockConnectivity mockConnectivity;
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  setUp(() {
+    mockApiViewModel = MockApiViewModel();
+    mockConnectivity = MockConnectivity();
+  });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+  testWidgets('displays no internet dialog when there is no connectivity', (WidgetTester tester) async {
+    when(mockConnectivity.checkConnectivity())
+        .thenAnswer((_) async => ConnectivityResult.none);
+
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider<ApiViewModel>.value(value: mockApiViewModel),
+        ],
+        child: MaterialApp(
+          home: MyHomePage(),
+        ),
+      ),
+    );
+
+    await tester.pumpAndSettle();
+
+    expect(find.text("No Internet Connection"), findsOneWidget);
+    expect(find.text("Please check your internet connection and try again."), findsOneWidget);
+  });
+
+  testWidgets('fetches data when there is internet connectivity', (WidgetTester tester) async {
+    when(mockConnectivity.checkConnectivity())
+        .thenAnswer((_) async => ConnectivityResult.wifi);
+    when(mockApiViewModel.fetchData())
+        .thenAnswer((_) async {});
+
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider<ApiViewModel>.value(value: mockApiViewModel),
+        ],
+        child: MaterialApp(
+          home: MyHomePage(),
+        ),
+      ),
+    );
+
+    await tester.pumpAndSettle();
+
+    verify(mockApiViewModel.fetchData()).called(1);
+  });
+
+  testWidgets('displays loading indicator when data is being fetched', (WidgetTester tester) async {
+    when(mockApiViewModel.isLoading).thenReturn(true);
+
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider<ApiViewModel>.value(value: mockApiViewModel),
+        ],
+        child: MaterialApp(
+          home: MyHomePage(),
+        ),
+      ),
+    );
+
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+
+  testWidgets('displays no data available when there is no data', (WidgetTester tester) async {
+    when(mockApiViewModel.isLoading).thenReturn(false);
+    when(mockApiViewModel.data).thenReturn([]);
+
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider<ApiViewModel>.value(value: mockApiViewModel),
+        ],
+        child: MaterialApp(
+          home: MyHomePage(),
+        ),
+      ),
+    );
+
+    expect(find.text('No data available'), findsOneWidget);
+  });
+
+  testWidgets('sorts countries by name when sort by name is tapped', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider<ApiViewModel>.value(value: mockApiViewModel),
+        ],
+        child: MaterialApp(
+          home: MyHomePage(),
+        ),
+      ),
+    );
+
+    await tester.tap(find.text('Sort By Name'));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    verify(mockApiViewModel.sortCountriesAlphabetically()).called(1);
   });
 }
